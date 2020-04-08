ceccf42fdbf099e0ceb8d5399a27f5650bdfa2eb
Lukas Lysychas <sayshi108@gmail.com>
2020-04-08 13:59:43 +0300

    Updates

---
+++ precache-manifest.4612fb5241e8b6e1bb02e2c935fabc3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46825dfb32391814dc9262b7288690a4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6e84340e40b08a33e26e",
+    "url": "/robofriends/static/css/main.af99f7f8.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e84340e40b08a33e26e",
+    "url": "/robofriends/static/js/main.2d0255a0.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
